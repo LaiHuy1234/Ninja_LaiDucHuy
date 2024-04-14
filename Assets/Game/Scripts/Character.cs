7705520186ce491d7b98a817b3e1eaf872ea013b
LaiHuy1234 <huylaitk@gmail.com>
2024-04-15 02:51:46 +0700

    First Demo

---
+++ Assets/Game/Scripts/Character.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+    [SerializeField] protected HealthBar healthBar;
+
+    [SerializeField] protected CombatText CombatTextPrefab;
+    private float hp;
+    private string currentAnimName;
+
+
+    public bool IsDead => hp <= 0;
+
+    private void Start()
+    {
+        OnInit();
+    }
+
+    public virtual void OnInit()
+    {
+        //healthBar.OnInit(100);
+        hp = 100;
+        healthBar.OnInit(100, transform);
+    }
+    public virtual void OnDespawm()
+    {
+
+    }
+
+    protected virtual void OnDeath()
+    {
+        ChangeAnim("die");
+        Invoke(nameof(OnDespawm), 2f);
+    }
+
+    protected void ChangeAnim(string animName)
+    {
+        if (currentAnimName != animName)
+        {
+            anim.ResetTrigger(animName);
+
+            currentAnimName = animName;
+
+            anim.SetTrigger(currentAnimName);
+        }
+    }
+
+    public void OnHit(float damage)
+    {
+        if (!IsDead)
+        {
+            hp -= damage;
+
+
+            if ( hp <= damage )
+            {
+                hp = 0;
+                OnDeath();
+            }
+
+            healthBar.SetNewHp(hp);
+            Instantiate(CombatTextPrefab, transform.position + Vector3.up, transform.rotation).OnInit(damage);
+        }
+    }
+
+}
+
